f559011bcc365709479660cf002a89f173c57c9d
Wellington Rutes <netorutes@gmail.com>
2018-09-01 18:33:22 -0300

    Created endpoint to get auth token.

--- voting/urls.py
+++ voting/urls.py
@@ -20,8 +20,11 @@ from django.urls import path
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('election.urls')),
     url(r'^api/v1/', include('user.urls')),
+    url(r'auth/token/', obtain_auth_token)
 ]

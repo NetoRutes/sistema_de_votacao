579a38bde1f6e2523ed2bdda92dfb5920b873798
Wellington Rutes <netorutes@gmail.com>
2018-09-02 16:48:36 -0300

    Updated election and candidates with manyTomany relation.

--- election/models.py
+++ election/models.py
@@ -12,7 +12,7 @@ class Election(models.Model):
     label = models.CharField(max_length=50)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    candidates = models.ForeignKey('election.Candidate', on_delete=models.PROTECT)
+    candidates = models.ManyToManyField('election.Candidate')
 
     def __str__(self):
         return self.label
